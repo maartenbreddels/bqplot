235fc1655b6004e2bd594672a7114a0221abc58f
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-03 15:01:58 +0100

    test(js,figure): add min and max aspect tests
    Signed-off-by: Maarten A. Breddels <maartenbreddels@gmail.com>

--- js/src/test/figure.ts
+++ js/src/test/figure.ts
@@ -49,4 +49,65 @@ describe("figure >", () => {
         const pixel = context.getImageData(test_x, test_y, 1, 1);
         expect(Array.prototype.slice.call(pixel.data)).to.deep.equals(pixel_red);
     });
+
+    it("min aspect check", async function() {
+        const x = {dtype: 'float32', value: new DataView((new Float32Array([0.5, 0.5])).buffer)};
+        const y = {dtype: 'float32', value: new DataView((new Float32Array([2.0, 2.5])).buffer)};
+        const { figure } = await create_figure_scatter(this.manager, x, y);
+
+        // the dom is 400x500
+        expect(figure.width).to.be.equals(400);
+        expect(figure.height).to.be.equals(500);
+
+        let width
+        let height;
+        // current aspect is 400/500 ~= 0.8 < 2, so we expect the height to decrease
+        figure.model.set('min_aspect_ratio', 2);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(400);
+        expect(height).to.be.equals(200);
+
+        // relaxing it, we expect it to grow again
+        figure.model.set('min_aspect_ratio', 1.0);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(400);
+        expect(height).to.be.equals(400);
+
+        // and back to its original size
+        figure.model.set('min_aspect_ratio', 0.1);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(400);
+        expect(height).to.be.equals(500);
+    });
+
+    it("max aspect check", async function() {
+        const x = {dtype: 'float32', value: new DataView((new Float32Array([0.5, 0.5])).buffer)};
+        const y = {dtype: 'float32', value: new DataView((new Float32Array([2.0, 2.5])).buffer)};
+        const { figure } = await create_figure_scatter(this.manager, x, y);
+
+        // the dom is 400x500
+        expect(figure.width).to.be.equals(400);
+        expect(figure.height).to.be.equals(500);
+
+        let width
+        let height;
+        // current aspect is 400/500 ~= 0.8 > 2, so we expect the width to decrease
+        figure.model.set('max_aspect_ratio', 0.5);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(250);
+        expect(height).to.be.equals(500);
+
+        // relaxing it, we expect it to grow again
+        figure.model.set('max_aspect_ratio', 0.6);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(300);
+        expect(height).to.be.equals(500);
+
+        // and back to its original size
+        figure.model.set('max_aspect_ratio', 1);
+        ({width, height} = figure.getFigureSize());
+        expect(width).to.be.equals(400);
+        expect(height).to.be.equals(500);
+    });
+
 });
